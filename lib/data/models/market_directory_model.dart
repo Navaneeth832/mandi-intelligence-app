ab5eafdaef860b5ae3d86cd80bd891ac1805021f
Navaneeth832 <tve23cs099@cet.ac.in>
2026-06-18 22:28:11 +0530

    complex market screen removed

--- lib/data/models/market_directory_model.dart
+++ lib/data/models/market_directory_model.dart
@@ -3,18 +3,12 @@ class MarketDirectory {
   final String name;
   final String district;
   final String state;
-  final List<String> commodities;
-  final int commodityCount;
-  final int totalRecords;
 
   MarketDirectory({
     required this.id,
     required this.name,
     required this.district,
     required this.state,
-    required this.commodities,
-    required this.commodityCount,
-    required this.totalRecords,
   });
 
   factory MarketDirectory.fromJson(Map<String, dynamic> json) {
@@ -23,9 +17,6 @@ class MarketDirectory {
       name: json['name'],
       district: json['district'],
       state: json['state'],
-      commodities: List<String>.from(json['commodities']),
-      commodityCount: json['commodity_count'],
-      totalRecords: json['total_records'],
     );
   }
 }
