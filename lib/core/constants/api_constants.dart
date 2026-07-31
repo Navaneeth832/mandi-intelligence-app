227014a9541e79e12e1abfa07d1cd871385dc939
Navaneeth832 <tve23cs099@cet.ac.in>
2026-07-31 21:34:27 +0530

    api constant change

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -2,6 +2,7 @@
 class ApiConstants {
   static const String baseUrl = 'https://mandi-intelligence-app.onrender.com';
   //static const String baseUrl = 'http://192.168.29.253:8000';
+  //static const String baseUrl = 'http://127.0.0.1:8000';
   static const String mandiPricesEndpoint = '/v1/mandi-prices';
   static const int timeoutSeconds = 30;
 }
